99c7547488dcf362190750fb2b3f81dfe9febb69
StrongCatWill <ssyysskywalker@gamil.com>
2022-11-29 21:55:38 +0900

    수정사항 1. hobby01, Hobby01.java, Hobby01_02, Hobby01_02.java 레이아웃 만들고 이미지뷰에 버튼 이벤트 달아 Dialog 띄움. 세부 설명은 추후에 추가.
    수정사항 2.
    앱 아이콘 f14 icon으로 변경

--- app/src/main/java/com/stringcatwill/mobile_programming_202041007/Hobby01.java
+++ app/src/main/java/com/stringcatwill/mobile_programming_202041007/Hobby01.java
@@ -1,26 +1,189 @@
 package com.stringcatwill.mobile_programming_202041007;
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.Typeface;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import androidx.annotation.Nullable;
 
-public class Hobby01 extends Activity{
+
+
+public class Hobby01 extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hobby01);
+
+        /*포스터 누르면 해당 작품의 코멘트가 담긴 팝업창 만들기
+        적용 범위(musical_01~mov_02)
+        참고한 웹페이지 (https://whddn528.tistory.com/11)
+        * */
+
+
+        //클릭 이벤트를 달 이미지 변수 이름은 id와 통일.
+        //뮤지컬 카테고리 > 브라더스 카라마죠프 창.
+        ImageView musical_01;
+        musical_01 = (ImageView)findViewById(R.id.musical_01);
+        musical_01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder musical_01_ad = new AlertDialog.Builder(Hobby01.this);
+
+                musical_01_ad.setTitle("뮤지컬 [브라더스 까라마죠프]");
+                musical_01_ad.setMessage("카라마죠프의 형제들을 원작으로 한 뮤지컬. \n 서술자 알료샤를 앞세운" +
+                        "원작 소설과 달리 이반과 스메르자코프의 갈등 관계를 주로 다루며, 성가를 변형한 넘버가 도드라짐.");
+                final EditText et = new EditText(Hobby01.this);
+                musical_01_ad.setView(et);
+
+                //닫기 버튼 만들기
+                musical_01_ad.setNegativeButton("닫기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+
+
+        //뮤지컬 카테고리 > 배니싱
+        ImageView musical_02;
+        musical_02 = (ImageView)findViewById(R.id.musical_02);
+        musical_02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder musical_02_ad = new AlertDialog.Builder(Hobby01.this);
+
+                musical_02_ad.setTitle("뮤지컬 [배니싱]");
+                musical_02_ad.setMessage("경성을 배경으로 하는 남성 3인 뮤지컬\n" +
+                        "최근에 제일 많이 본 작품입니다." +
+                        "핵심 대사"+
+                        "'피부로 나뉘어 있는 한 우리는 영원히 타인의 감각을 이해할 수 없을 것이다.\n" +
+                        "피부를 뚫고, 그와 내가 하나의 동질임을 깨달았을 때, 비로소 이해가 시작된다.' \n" +
+                        "의학도와 우생학을 연구하는 친일파, 벰파이어가 등장하지만 본질은 이해에 관한 극." +
+                        "무대가 아름답고 넘버가 극적이에요. ");
+                final EditText et = new EditText(Hobby01.this);
+                musical_02_ad.setView(et);
+
+                //닫기 버튼 만들기
+                musical_02_ad.setNegativeButton("닫기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+        //뮤지컬 카테고리 > 땡큐 베리 스트로베리
+        ImageView musical_03;
+        musical_03 = (ImageView)findViewById(R.id.musical_03);
+        musical_03.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder musical_03_ad = new AlertDialog.Builder(Hobby01.this);
+
+                musical_03_ad.setTitle("뮤지컬 [땡큐 베리 스트로베리]");
+                musical_03_ad.setMessage("설명을 적어라 오타쿠");
+                final EditText et = new EditText(Hobby01.this);
+                musical_03_ad.setView(et);
+
+                //닫기 버튼 만들기
+                musical_03_ad.setNegativeButton("닫기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+        //뮤지컬 카테고리 > 지저스 크라이스트 슈퍼스타
+        ImageView musical_04;
+        musical_04 = (ImageView)findViewById(R.id.musical_04);
+        musical_04.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder musical_04_ad = new AlertDialog.Builder(Hobby01.this);
+
+                musical_04_ad.setTitle("뮤지컬 [지저스 크라이스트 슈퍼스타]");
+                musical_04_ad.setMessage("설명을 적어라 오타쿠");
+                final EditText et = new EditText(Hobby01.this);
+                musical_04_ad.setView(et);
+
+                //닫기 버튼 만들기
+                musical_04_ad.setNegativeButton("닫기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+        
+        //영화 카테고리 > 캡틴 아메리카 윈터솔저
+        ImageView mov_01;
+        mov_01 = (ImageView)findViewById(R.id.mov_01);
+        mov_01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mov_01_ad = new AlertDialog.Builder(Hobby01.this);
+
+                mov_01_ad.setTitle("영화 [캡틴 아메리카 윈터솔저]");
+                mov_01_ad.setMessage("설명을 적어라 오타쿠");
+                final EditText et = new EditText(Hobby01.this);
+                mov_01_ad.setView(et);
+
+                //닫기 버튼 만들기
+                mov_01_ad.setNegativeButton("닫기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+
+        //영화 카테고리 > 포레스트 검프
+        ImageView mov_02;
+        mov_02 = (ImageView)findViewById(R.id.mov_02);
+        mov_02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mov_02_ad = new AlertDialog.Builder(Hobby01.this);
+
+                mov_02_ad.setTitle("영화 [포레스트 검프]");
+                mov_02_ad.setMessage("설명을 적어라 오타쿠");
+                final EditText et = new EditText(Hobby01.this);
+                mov_02_ad.setView(et);
+
+                //닫기 버튼 만들기
+                mov_02_ad.setNegativeButton("닫기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+
+
+        //다음 페이지로 이동하는 버튼 만들기.
+        Button btn_nextPage = (Button)findViewById(R.id.btn_nextPage);
+        btn_nextPage.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //화면 전환
+                Intent intent = new Intent(getApplicationContext(), Hobby01_02.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/stringcatwill/mobile_programming_202041007/Hobby01_02.java
+++ app/src/main/java/com/stringcatwill/mobile_programming_202041007/Hobby01_02.java
@@ -1,21 +1,14 @@
 package com.stringcatwill.mobile_programming_202041007;
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.Typeface;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import androidx.annotation.Nullable;
+
 
 public class Hobby01_02 extends Activity{
 
@@ -23,5 +16,168 @@ public class Hobby01_02 extends Activity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hobby01_02);
+
+                /*포스터 누르면 해당 작품의 코멘트가 담긴 팝업창 만들기
+        적용 범위(mov_03~mov_08)
+        참고한 웹페이지 (https://whddn528.tistory.com/11)
+        * */
+
+        //영화 카테고리 > 오만과 편견
+        ImageView mov_03;
+        mov_03 = (ImageView)findViewById(R.id.mov_03);
+        mov_03.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mov_03_ad = new AlertDialog.Builder(Hobby01_02.this);
+
+                mov_03_ad.setTitle("영화 [오만과 편견]");
+                mov_03_ad.setMessage("설명을 적어라 오타쿠");
+                final EditText et = new EditText(Hobby01_02.this);
+                mov_03_ad.setView(et);
+
+                //닫기 버튼 만들기
+                mov_03_ad.setNegativeButton("닫기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+
+        //영화 카테고리 > 스타워즈 시스의 복수
+        ImageView mov_04;
+        mov_04 = (ImageView)findViewById(R.id.mov_04);
+        mov_04.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mov_04_ad = new AlertDialog.Builder(Hobby01_02.this);
+
+                mov_04_ad.setTitle("영화 [스타워즈 ep3 시스의 복수]");
+                mov_04_ad.setMessage("설명을 적어라 오타쿠");
+                final EditText et = new EditText(Hobby01_02.this);
+                mov_04_ad.setView(et);
+
+                //닫기 버튼 만들기
+                mov_04_ad.setNegativeButton("닫기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+
+        //영화 카테고리 > 반지의 제왕 두 개의 탑
+        ImageView mov_05;
+        mov_05 = (ImageView)findViewById(R.id.mov_05);
+        mov_05.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mov_05_ad = new AlertDialog.Builder(Hobby01_02.this);
+
+                mov_05_ad.setTitle("영화 [반지의 제왕 - 두 개의 탑]");
+                mov_05_ad.setMessage("설명을 적어라 오타쿠");
+                final EditText et = new EditText(Hobby01_02.this);
+                mov_05_ad.setView(et);
+
+                //닫기 버튼 만들기
+                mov_05_ad.setNegativeButton("닫기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+        
+        //영화 카테고리 > 월플라워
+        ImageView mov_06;
+        mov_06 = (ImageView)findViewById(R.id.mov_06);
+        mov_06.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mov_06_ad = new AlertDialog.Builder(Hobby01_02.this);
+
+                mov_06_ad.setTitle("영화 [월플라워]");
+                mov_06_ad.setMessage("설명을 적어라 오타쿠");
+                final EditText et = new EditText(Hobby01_02.this);
+                mov_06_ad.setView(et);
+
+                //닫기 버튼 만들기
+                mov_06_ad.setNegativeButton("닫기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+
+        //영화 카테고리 > 검은사제들
+        ImageView mov_07;
+        mov_07 = (ImageView)findViewById(R.id.mov_07);
+        mov_07.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mov_07_ad = new AlertDialog.Builder(Hobby01_02.this);
+
+                mov_07_ad.setTitle("영화 [검은사제들]");
+                mov_07_ad.setMessage("설명을 적어라 오타쿠");
+                final EditText et = new EditText(Hobby01_02.this);
+                mov_07_ad.setView(et);
+
+                //닫기 버튼 만들기
+                mov_07_ad.setNegativeButton("닫기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+
+        //영화 카테고리 > 넷플릭스 앤
+        ImageView mov_08;
+        mov_08 = (ImageView)findViewById(R.id.mov_08);
+        mov_08.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mov_08_ad = new AlertDialog.Builder(Hobby01_02.this);
+
+                mov_08_ad.setTitle("넷플릭스 드라마 [빨강머리 앤]");
+                mov_08_ad.setMessage("설명을 적어라 오타쿠");
+                final EditText et = new EditText(Hobby01_02.this);
+                mov_08_ad.setView(et);
+
+                //닫기 버튼 만들기
+                mov_08_ad.setNegativeButton("닫기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+
+
+
+        //다음 페이지로 이동하는 버튼 만들기.
+        Button btn_backHome = (Button)findViewById(R.id.btn_backHome);
+        btn_backHome.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //메인 화면으로 이동.
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
